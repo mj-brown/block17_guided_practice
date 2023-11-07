e5cc6908017cc7a7f6aa0b4bdd34ce5d03e698d4
mj-brown <brown8mj5@gmail.com>
2023-11-07 13:26:50 -1000

    initial commit and completion of guided practice

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # block17_guided_practice
+
+This repo contains the guided practice for Unit 2 Block 17.  The guided practice focusses on creating and modifying classes.
---
+++ sentence.js
@@ -0,0 +1,42 @@
+class Sentence {
+    constructor(data) {
+        this.data = data;
+    }
+    wordCount () {
+        return this.data.split(' ').length;
+    }
+    hasLetter () {
+        return this.data.indexOf(letter) !== -1;
+    }
+    letterCount () {
+        let count = 0;
+        for(let i = 0; i < this.data.length; i++) {
+            if(this.data[i] === letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+    stats() {
+        const obj = {};
+        const words = this.data.split(' ');
+        words.forEach(word => {
+            if (obj[word] === undefined) {
+                obj[word] = 0;
+            }
+            obj[word]++;
+        });
+        return obj;
+    }
+};
+const data = //prompt(
+    //'Enter in a sentence',
+    'The cow jumped over the moon'
+//);
+
+const s1 = new Sentence(data);
+console.log(s1.wordCount());
+console.log(s1.hasLetter("Y"));
+console.log(s1.hasLetter('c'));
+console.log(s1.letterCount('e'));
+console.log(s1.letterCount(' '));
